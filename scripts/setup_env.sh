c6a268acd1f507df89e833428ae37c4015db8a78
Hitoshi Amano <seijro@gmail.com>
2023-08-15 14:51:44 +0900

    .

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -10,10 +10,6 @@ cd ~/dev
 
 python3.10 -m venv venv
 venv/bin/pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-venv/bin/pip install transformers
-venv/bin/pip install sentencepiece einops
-
-# huggingface の authz がいるので access token を入れる
-venv/bin/pip install huggingface_hub
+venv/bin/pip install transformers sentencepiece einops huggingface_hub
 venv/bin/huggingface-cli login
 
