bd10dd9d2a1c632cb26dd71f37dfa5bf574ae85d
Hitoshi Amano <seijro@gmail.com>
2023-08-15 13:16:13 +0900

    .

--- scripts/setup_instance/a100_40/00_setup_instance.sh
+++ scripts/setup_instance/a100_40/00_setup_instance.sh
@@ -8,7 +8,7 @@ sudo apt upgrade -y
 
 # 朝 6 時に起動してたら必ず終了する（お金がもったいないので）
 sudo timedatectl set-timezone Asia/Tokyo
-sudo sh -c 'crontab -l > /tmp/current_cron'
+sudo sh -c 'crontab -l > /tmp/current_cron' || : # ignore error
 sudo sh -c 'echo "0 6 * * * /sbin/shutdown -h now" >> /tmp/current_cron'
 sudo crontab /tmp/current_cron
 sudo systemctl restart cron
